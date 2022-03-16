1c9ec2f8de1716ed8b048c99e761a83acf0dc9de
Justin Abraham Ipe <justinipe8k@gmail.com>
2022-03-16 12:51:21 +0000

    new code added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react"
+import {Switch, Route} from 'react-router-dom'
+import {Component} from 'react'
+import Home from './components/Home'
+import About from './components/About'
+import ThemeContext from './context/ThemeContext'
+import NotFound from './components/NotFound'
+import './App.css'
 
-export default function App() {
+class App extends Component {
+  state = {isDarkTheme: false}
+
+  toggleTheme = () =>
+    this.setState(prevState => ({isDarkTheme: !prevState.isDarkTheme}))
+
+  render() {
+    const {isDarkTheme} = this.state
+
     return (
-         <div> 
-       Hello world!! ha ha
-         </div>     
+      <ThemeContext.Provider
+        value={{isDarkTheme, toggleTheme: this.toggleTheme}}
+      >
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </Switch>
+      </ThemeContext.Provider>
     )
+  }
 }
+
+export default App
